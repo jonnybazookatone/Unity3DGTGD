3794a71f5567a1b254704294a3687cf6ca3f5a67
Jonny Elliott <jonnynelliott@googlemail.com>
2014-02-26 23:24:43 +0100

    GTGD s1.5 SpawnManager, file added

---
+++ SpawnScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This script is attached to the SpawnManager and it allows
+/// the player to spawn in to the multiplayer game.
+/// </summary>
+
+public class SpawnScript : MonoBehaviour {
+
+	// Variables start____________________
+	// 011:02
+	// Variables end______________________
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
