0fc047a13e29198c09549cdef4ffb847bf0df589
Jonny Elliott <jonnynelliott@googlemail.com>
2014-03-13 08:59:52 +0100

    Statistics display and crosshair scripts added for lesson 1.7. Comments updated in HealthAndDamage

---
+++ Crosshair.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This script is attached to the player
+/// and causes a crosshair to be drawn
+/// to the screen
+/// 
+/// All code is taken from Gamer To Game Developer Series 1
+/// http://www.gamertogamedeveloper.com
+/// No credit is taken myself
+/// </summary>
+
+public class Crosshair : MonoBehaviour {
+
+	// Variables start__________________
+	public Texture crosshairTex;
+	private float crosshairDimension = 256;
+	public float rectHeight;
+	public float rectWidth;
+
+	// Variables end____________________
+
+	// Use this for initialization
+	void Start () 
+	{
+		if(networkView.isMine == false)
+		{
+			enabled = false;
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI ()
+	{
+		// Display the crosshair in the center of the screen
+		// while the cursor is locked
+		if(Screen.lockCursor == true)
+		{
+			rectHeight = (Screen.height - crosshairDimension) / 2;
+			rectWidth = (Screen.width - crosshairDimension) / 2;
+			GUI.DrawTexture(new Rect(rectWidth, rectHeight, crosshairDimension, crosshairDimension),
+			                crosshairTex);
+		}
+	}
+}
---
+++ StatDisplay.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This script is attached to the player
+/// and allows the player to see a box with their
+/// current health to the lower right of the crosshair
+/// 
+/// This script accesses the HealthAndDamage script
+/// 
+/// All code is taken from Gamer To Game Developer Series 1
+/// http://www.gamertogamedeveloper.com
+/// No credit is taken myself
+/// </summary>
+
+public class StatDisplay : MonoBehaviour {
+
+	// Variables start____________________
+
+	// The healthbar texture is attached to this 
+	// in the inspector
+	public Texture healthTex;
+
+	// These are used in calculating and displaying
+	// the health
+	private float health;
+	private int healthForDisplay;
+
+	// These are used in defining the StatDisplay box
+	private int boxWidth = 160;
+	private int boxHeight = 85;
+	private int labelHeight = 20;
+	private int labelWidth = 35;
+	private int padding = 10;
+	private int gap = 120;
+	private float healthBarLength;
+	private int healthBarHeight = 15;
+	private GUIStyle healthStyle = new GUIStyle();
+	private float commonLeft;
+	private float commonTop;
+
+	// A quick reference to the HealthAndDamage script
+	private HealthAndDamage HDScript;
+
+	// Variables end______________________
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		if(networkView.isMine == true)
+		{
+			// Access the HealthAndDamage script
+			Transform triggerTransform = transform.FindChild("Trigger");
+			HDScript = triggerTransform.GetComponent<HealthAndDamage>();
+
+			// Set the GUI style
+			healthStyle.normal.textColor = Color.green;
+			healthStyle.fontStyle = FontStyle.Bold;
+		}
+		else
+		{
+			enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// Access the HealthAndDamage script continuously and retrieve
+		// the player's current health
+		health = HDScript.myHealth;
+
+		// I also want to display health as a number, without any decimals
+		healthForDisplay = Mathf.CeilToInt(health);
+
+		// Calculate how long the health bar should be. The max length is 100
+		// representing 100 percent
+		healthBarLength = (health / HDScript.maxHealth) * 100;
+	}
+
+	void OnGUI ()
+	{
+		commonLeft = (Screen.width / 2) + 180;
+		commonTop = (Screen.height / 2) + 50;
+
+		// Draw a plane box behind the health bar
+		GUI.Box(new Rect(commonLeft, commonTop, boxWidth, boxHeight), "");
+
+		// Draw a grey box behind the health bar 
+		GUI.Box(new Rect(commonLeft+padding, commonTop+padding, 100, healthBarHeight), ""); 
+
+	}
+}
